c9fa329c430e65aff140fa7a97fb789ea145c694
MasterZense <50195844+MasterZense@users.noreply.github.com>
2023-05-21 08:16:11 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,9 +267,9 @@ app.listen(port, () => {
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
-    const message = 'Welcome to the GogoAnime bot! How can I assist you?';
+    const message = 'Welcome to the AnimeYay bot! Here's some guide to get you started.\n\n /search <Keyword> Search your favorite Anime By Title\n /episodes <AnimeId> Enter AnimeId to show list of episodes of that anime.\n /watch <episodeId> enter episodeId to show video url to watch.\n\n <b>Recommended Video Players to Use</b>\n\n 1. MXPlayer\n 2. VLC For Android';
   
-    bot.sendMessage(chatId, message);
+    bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
 });
 
 bot.onText(/\/search (.+)/, async (msg, match) => {
