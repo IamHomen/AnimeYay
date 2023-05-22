60dfc7b192a0f0c916d43fb4b01522cbc13c3dd1
MasterZense <50195844+MasterZense@users.noreply.github.com>
2023-05-22 15:02:44 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -626,7 +626,7 @@ bot.onText(/\/download (.+)/, async (msg, match) => {
 
     const links = await scrapeDownload({ id: episodeId });
     const chatId = msg.chat.id;
-    let response = '<b>Download Links</b>\n\n <em>This links expires in couples of hours</em>';
+    let response = '<b>Download Links</b>\n\n <em>This links expires in couples of hours</em>\n\n';
     
     links.forEach(link => {
 
